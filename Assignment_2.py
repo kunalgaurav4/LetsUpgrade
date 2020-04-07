96affa641f941d7ad58c68655bb09b12fa0499d4
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-04-07 23:46:51 +0530

    second assignment

---
+++ Assignment_2.py
@@ -0,0 +1,35 @@
+# Basi operation
+a =7
+b = 9.6
+print(a+b)
+
+
+print(int(3.56))
+
+# Conacatenation
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+
+# Slicing a List
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[2:])
+
+
+ 
+a_tuple = (10,20,30,40,50,60)
+ 
+obj_slice = slice(4)
+ 
+ 
+ 
+print("The tuple after slice:" ,a_tuple[obj_slice])
